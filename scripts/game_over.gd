0a2ef1eee7b29457f4c6042015f36725e63da39b
Ricardo Dias <rricardodias99@gmail.com>
2024-11-08 21:46:10 -0300

    Atualização da cena main

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -1,10 +1,9 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	hide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
